bd8e178e307b19a27547f4d3dcae05f572eab9f1
jalvarezc23 <jalvarezc23@fpcoruna.afundacion.org>
2026-04-16 16:56:29 +0200

    url de get counter

--- python/ContApp/ContApp/urls.py
+++ python/ContApp/ContApp/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from cont01app.views import create_counter, get_counter_stats, update_counter, delete_counter, increment_counter
+from cont01app.views import create_counter, get_counter_stats, update_counter, delete_counter, increment_counter, \
+    get_counter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('counters/', get_counter),
     path('counters/<int:counter_id>/stats/', get_counter_stats),
     path('counters/create/', create_counter),
     path('counters/<int:counter_id>/update/', update_counter),
